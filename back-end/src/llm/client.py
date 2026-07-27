291c0022382ff675dbb7dc060b287f9148e59732
Paul Tran <paultran@Pauls-MacBook-Air.local>
2026-07-27 18:40:50 -0400

    Refactored code, separated workload into separate folders

--- back-end/src/llm/client.py
+++ back-end/src/llm/client.py
@@ -1,20 +1,3 @@
-from langchain_core.prompts import ChatPromptTemplate, prompt
 from langchain_ollama.llms import OllamaLLM
-import asyncio
-
+ 
 llm = OllamaLLM(model="llama3.1", temperature=0)
-
-async def handle_query(query):
-    template = ChatPromptTemplate.from_messages([
-        ("system", "You are a helpful and kind AI assistant who enjoys responding in a helpful tone"),
-        ("human", "{question}")
-        ])
-    
-    prompt = template.invoke({"question": query})
-
-    response = await llm.ainvoke(prompt)
-
-    print(response)
-
-if __name__ == "__main__":
-    asyncio.run(handle_query("What is React?"))
