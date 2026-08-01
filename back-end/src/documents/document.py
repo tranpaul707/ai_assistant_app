09131adf92bed347ce997b782169fbe08b4c1cca
Paul Tran <tranpaul770@gmail.com>
2026-08-01 00:44:57 -0400

    Basic load_file implementation for document types

---
+++ back-end/src/documents/document.py
@@ -0,0 +1,23 @@
+import pathlib
+from langchain_community.document_loaders import (
+    PyPDFLoader,
+    TextLoader,
+    Docx2txtLoader,
+)
+
+def load_file(file):
+    file_extension = pathlib.Path(file).suffix
+
+    if file_extension == ".txt":
+        loader = PyPDFLoader(file)
+
+    elif file_extension == ".pdf":
+        loader = TextLoader(file)
+
+    elif file_extension == ".docx":
+        loader = Docx2txtLoader(file)
+
+    else:
+        raise ValueError(f"Unsupported file type: {file_extension}")
+
+    return loader.load()
