cc5c1416a96ed3e8b154510185e5373fb2834a33
Paul Tran <tranpaul770@gmail.com>
2026-08-31 22:31:52 -0400

    Implemented PdfReader for uploaded files

--- back-end/src/api/routes/upload.py
+++ back-end/src/api/routes/upload.py
@@ -1,6 +1,8 @@
 from pathlib import Path
-
+from io import BytesIO
 from fastapi import APIRouter, File, UploadFile
+import io
+from pypdf import PdfReader
 
 router = APIRouter()
 
@@ -9,4 +11,13 @@ UPLOAD_DIR = Path(__file__).resolve().parent.parent.parent
 
 @router.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    print("Request Received")
+   data = await file.read()
+   buffer = io.BytesIO(data)
+   pdf_text = PdfReader(buffer)
+
+   for page in pdf_text.pages:
+    print(page.extract_text())
+
+
+
+   
